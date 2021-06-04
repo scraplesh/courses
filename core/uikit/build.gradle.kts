ab7f9b2bd738f7042e724abc4c6f07d9b95094b5
Artem Tomilov <scraplesh@gmail.com>
2021-06-04 22:34:35 +0300

    Splash screen

--- core/uikit/build.gradle.kts
+++ core/uikit/build.gradle.kts
@@ -1,13 +1,9 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("kotlin-kapt")
-    id("kotlin-android-extensions")
 }
 
 dependencies {
-    implementation(project(":domain"))
     implementation(Deps.androidxConstraintLayout)
-    implementation(Deps.androidxFragmentKtx)
     implementation(Deps.material)
 }
